5fee32e80f644d6e6249e11dbff9723fba512c89
dudisnm <00001102035038sp@al.educacao.sp.gov.br>
2024-11-26 13:50:02 -0300

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografua',
+    'Em qual paÌs está localizado o maior prédio do mundo?',
+    'Dubai'
+)
+
+criaCartao(
+    'Tecnologia',
+    'O que é maior gigabayte ou megabayte?',
+    'Gigabayte'
+)
+
+criaCartao(
+    'Geografia',
+    'Quais são as duas linguas mas faladas no mundo?',
+    'Inglês e manadarim chinês'
+)
+
+criaCartao(
+    'Astrologia',
+    'Quais são as fases da lua?',
+    'Nova, crescente, minguante, cheia'
+)
